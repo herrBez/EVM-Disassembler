a20b7939a30b28907e2cc1eb0fc9ff638422a975
herrBez <bez.mirko@gmail.com>
2018-09-12 12:11:23 +0200

    Add a single junit test to discover errors

---
+++ src/test/StringOutputTargetTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import main.Parser;
+import main.StringOutputTarget;
+import main.Target;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringOutputTargetTest {
+
+    @Test
+    void Oracle() throws Exception {
+        /* Read the expected output */
+        Scanner s = new Scanner(new File("./src/test/Test1_expectedOutput.txt"));
+        StringBuilder sb = new StringBuilder();
+        while(s.hasNextLine()) {
+            sb.append(s.nextLine() + "\n");
+        }
+        String expectedOutput = sb.toString();
+        /* Read the input */
+        String input = new Scanner(
+                new File("./src/test/Test1_Input.txt"))
+                .nextLine();
+        /* Generate the code */
+        short [] code = Parser.toShortArray(input);
+        Target t = new StringOutputTarget();
+        assertEquals(expectedOutput, t.output(code));
+
+    }
+
+}
